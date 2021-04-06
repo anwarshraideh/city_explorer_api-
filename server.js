828e8963df1e3e2ba26d1715c93b36906cf9b4d5
anwarshraideh <ahshraida@gmail.com>
2021-04-06 08:54:08 +0300

    lab7

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ server.use(cors());
 server.get('/', homeRouteHandler);
 server.get('/location', locationHandler);
 server.get('/weather', weatherHandler);
-// server.get('/parks', parksHandler);
+server.get('/parks', parksHandler);
 server.get('*', notFoundHandler);
 
 
@@ -32,21 +32,21 @@ function notFoundHandler(req,res){
   res.status(500).send(errObj);
 }
 
-// request url (browser): localhost:5000/location
+// request url (browser): localhost:3000/location
 function locationHandler(req,res){
 
   let cityName = req.query.city;
   let key = process.env.LOCATION_KEY;
   let LocationURL = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
 
-  superagent.get(LocationURL) 
+  superagent.get(LocationURL)
     .then(geoData => {
       console.log('inside superagent');
       console.log(geoData.body);
       let gData = geoData.body;
       const locationData = new Location(cityName, gData);
       res.send(locationData);
-      
+
     })
 
     .catch(error => {
@@ -72,13 +72,21 @@ function weatherHandler(req,res)
   let weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}&city=${cityn}`;
 
 
-  superagent.get(weatherURL) //send request to LocationIQ API
+  superagent.get(weatherURL)
     .then(weatherData => {
       let Data = weatherData.body.data;
-      let ArrOfWeather  = Data.map(value => {
-        return new Weather (value); });
+      let ArrOfWeather = Data.map(value => {
+        return new Weather (value);
+      });
       res.send(ArrOfWeather);
 
+    })
+
+
+    .catch(error => {
+      console.log('Error in getting data from LocationIQ server');
+      console.error(error);
+      res.send(error);
     });
 
 
@@ -86,6 +94,34 @@ function weatherHandler(req,res)
 }
 
 
+
+function parksHandler(req, res) {
+  console.log(req.query);
+
+  let cityname2 = req.query.search_query;
+  let key= process.env.PARK_API_KEY;
+  let parksURL = `https://developer.nps.gov/api/v1/parks?q=${cityname2}&api_key=${key}&limit=10`;
+
+
+  superagent.get(parksURL)
+    .then(item => {
+      let parkData = item.body.data.map(park => {
+        return new Park(park);
+      });
+      res.send(parkData);
+
+    })
+
+
+    .catch(error => {
+      console.log('Error in getting data from LocationIQ server');
+      console.error(error);
+      res.send(error);
+    });
+
+
+
+}
 
 
 
@@ -103,9 +139,13 @@ function Location(cityName, geoData) {
   this.longitude = geoData[0].lon;
 }
 
-
-
-
+function Park(data){
+  this.name = data.fullName;
+  this.address =`${data.addresses[0].line1}, ${data.addresses[0].stateCode}, ${data.addresses[0].city}`;
+  this.fee=data.entranceFees[0].cost;
+  this.description=data.description;
+  this.park_url=data.url;
+}
 
 
 server.listen(PORT,()=>{
